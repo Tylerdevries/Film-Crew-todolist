8fcbee75d0f3b4ac0bf5ee9a97a96c1eea65e32f
Tylerdevries <tylerdevries365@gmail.com>
2023-02-05 02:52:55 +0000

    Added personalization to previous project, just testing left to do before migrating to new host

--- todolistprj/filmcrewtodo/settings.py
+++ todolistprj/filmcrewtodo/settings.py
@@ -1,23 +1,20 @@
 from pathlib import Path
-import os 
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+SECRET_KEY = 'django-insecure--gg+d)g$50bvj$8q751ndoq*p)9t8m3f)#@@0l4vb!rk-!g^5e'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8(@pg5t6(($0c7=z_j$fn*6x*j)g6#0$6ziu77bks@pu0$-7zt'
+CSRF_TRUSTED_ORIGINS = ['https://8000-tylerdevrie-todolistprj-hqdrmugz94p.ws-eu85.gitpod.io']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-tylerdevrie-todolistprj-kg8jn5fppxz.ws-eu85.gitpod.io']
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,12 +36,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'todolistprj.urls'
+ROOT_URLCONF = 'filmcrewtodo.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -57,11 +54,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'todolistprj.wsgi.application'
+WSGI_APPLICATION = 'filmcrewtodo.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -70,9 +64,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,24 +81,20 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
